e7d8c58cdf48dea3f8edbfb576b5052fef8b7d6a
captain-pool <rickdey1998@gmail.com>
2019-03-01 01:54:21 +0530

    Adrish

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import numpy as np
 def get_score(slide_dict,prev_slide_num,next_slide_num):
     set1,set2 = set(slide_dict[prev_slide_num]),set(slide_dict[next_slide_num])
     return min(len(set1-set2),len(set1&set2),len(set2-set1))
@@ -6,7 +8,31 @@ def get_score(slide_dict,prev_slide_num,next_slide_num):
 #def maximize(dict):
 #   dict: {0:["tag1","tag2"],1:["tag3","tag4"]}
 #   returns {"ID1-ID2":score1, "ID2-ID3":score2}
-
+def sort_by_score(d):
+    return np.array(d).view('i8,i8,i8').sort(order = ["f2"],axis = 0)
+def arrange(v,h):
+    vl = sort_by_score(maximize(v))
+    
+def brute_force(horizontal, vertical):
+    frame = []
+    for item_id in horizontal:
+        f = [item_id]
+        frame.append(f)
+    l = 0
+    f = []
+    for item_id in vertical:
+        f.append(item_id)
+        l += 1
+        if l == 2:
+            l = 0
+            frame.append(f)
+            f = []
+    print(len(frame))
+    for item in frame:
+        if type(item) == type(""):
+            print(item)
+        else:
+            print(" ".join(list(map(str,item))))
 def input_(fn = ""):
     f = open(fn,"r")
     testcase = int(f.readline())
@@ -31,4 +57,5 @@ def input_(fn = ""):
             p+=1
     f.close()
     return horizontal,vertical
-h,v = input_("test.txt")
+h,v = input_(sys.argv[1])
+brute_force(h,v)
