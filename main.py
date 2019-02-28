1bc3508bcaa19d3d1b4a5e31dd4aa57fd39db56c
captain-pool <rickdey1998@gmail.com>
2019-03-01 00:17:51 +0530

    Adrish

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ def get_score(prev_slide_num,next_slide_num):
     set1,set2 = set(slide_dict[prev_slide_num]),set(slide_dict[next_slide_num])
     return min(len(set1-set2),len(set1&set2),len(set2-set1))
 def input_():
-    n = int(input())
-    photos = {}
-    for i in range(n):
-        inp = input().strip().split()
-        hv = inp[0]
-        tags = inp[-int(inp[1]):]
-        photos[i] = [hv,tags]
-    return (photos)
+    testcase = int(input())
+    horizontal={}
+    vertical={}
+    p,q=0,0
+    a,b=[],[]
+    while(testcase):
+        testcase-=1
+        photo = list(map(str,input().split()))
+        if(photo[0]=="H"):
+            for i in range(2,len(photo)):
+                a.append(photo[i])
+            horizontal[p]=a
+            a=[]
+            p+=1
+        if(photo[0]=="V"):
+            for i in range(2,len(photo)):
+                b.append(photo[i])
+            vertical[p]=b
+            b=[]
+            p+=1
 
+    return horizontal,vertical
