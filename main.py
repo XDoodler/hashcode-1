5133e4b24021e39413071ba920e89ff0d82e24fa
captain-pool <rickdey1998@gmail.com>
2019-02-28 23:58:08 +0530

    init commit

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-photo_dict = {0:["cat","beach","sun"],1:["selfie","smile"]}
+slide_dict = {0:["cat","beach","sun"],1:["selfie","smile"]}
+slides = []
 def get_score(prev_slide_num,next_slide_num):
-    set1,set2 = set(photo_dict[prev_slide_num]),set(photo_dict[next_slide_num])
+    set1,set2 = set(slide_dict[prev_slide_num]),set(slide_dict[next_slide_num])
     return min(len(set1-set2),len(set1&set2),len(set2-set1))
+def input_():
+    n = int(input())
+    photos = {}
+    for i in range(n):
+        inp = input().strip().split()
+        hv = inp[0]
+        tags = inp[-int(inp[1])]
+        photos[i] = [hv,tags]
+    print(photos)
+print(input_())
+
     
