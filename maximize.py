46b0c08b9f8382f9ad3c698d4bcf69a1d41348f6
Arkapravo Ghosh <arkapravoghosh98@gmail.com>
2019-03-01 02:53:25 +0530

    Update maximize.py

--- maximize.py
+++ maximize.py
@@ -16,6 +16,26 @@ def maximize(images):
 
 
 
+def maximize_h(images):
+    """
+    dict: {0:["tag1","tag2"], 1:["tag3","tag4"]}
+    returns [[ID1, ID2, score1], [ID2, ID3, score2]]
+    """
+    frames = []
+    l = []
+    d = {}
+    for item in images:
+        l.append(item)
+        d[item] = 1
+    for i in range(0, len(l), 2):
+        score = get_score(images, l[i], l[i+1])
+        frames.append([l[i], l[i+1], score])
+    if l%2 == 1:
+        frames.append([-1, l[len(l)-1], score])
+    return frames
+
+
+
 
 def maximize(images):
     """
